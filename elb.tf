42777cc1e5953ae1f517f0f8c71aa875cf3ecace
sujith19217 <119721320+sujith19217@users.noreply.github.com>
2023-11-10 19:13:20 +0530

    Update elb.tf

--- elb.tf
+++ elb.tf
@@ -1,5 +1,5 @@
 resource "aws_elb" "bar" {
-  name               = "raham-terraform-elb"
+  name               = "sujith-terraform-elb"
   availability_zones = ["ap-southeast-1a", "ap-southeast-1b"]
 
   listener {
@@ -21,7 +21,7 @@ resource "aws_elb" "bar" {
   cross_zone_load_balancing = true
   idle_timeout              = 400
   tags = {
-    Name = "raham-tf-elb"
+    Name = "sujith-tf-elb"
   }
 }
 
